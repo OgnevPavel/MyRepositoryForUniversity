b3fa9f9758f06e108d4de79929fd19a3f1e48188
OgnevPavel <ognev.pavel.94@mail.ru>
2016-03-29 13:30:32 +0300

    Update Main.java

--- Lab №3/src/ru/bstu/iitus/vt41/opn/Main.java
+++ Lab №3/src/ru/bstu/iitus/vt41/opn/Main.java
@@ -1,25 +1,14 @@
 package ru.bstu.iitus.vt41.opn;
 
-import com.sun.istack.internal.NotNull;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.List;
+import java.util.Scanner;
+import java.util.SortedSet;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static com.sun.deploy.util.StringUtils.splitString;
-import static java.nio.file.Files.readAllLines;
-import static java.util.Arrays.asList;
 
 /**
  * Created by KASPER on 18.03.2016.
@@ -31,12 +20,12 @@ public class Main {
             String inFilename = readString("Enter file name: ");
             String subString = readString("Enter sub string: ");
 
-            ProcessFile readFile = new ProcessFile(readAllLines(Paths.get(inFilename)));
+            ProcessFile readFile = new ProcessFile(readFileLines(inFilename));
             FutureTask<SortedSet<String>> processFile = buildFutureTask(readFile);
             SortedSet<String> setWord = processFile.get();
 
-            GetContains getContain = new GetContains(setWord, subString);
-            FutureTask<SortedSet<String>> getContains = buildFutureTask(getContain);
+            ProcessWords processWords = new ProcessWords(setWord, subString);
+            FutureTask<SortedSet<String>> getContains = buildFutureTask(processWords);
             SortedSet<String> setWordWithSubString = getContains.get();
 
             SetOutput setOutput = new SetOutput(setWordWithSubString);
@@ -54,26 +43,10 @@ public class Main {
         return task;
     }
 
-    /**
-     * Reads a text file and builds a string of its content. Returns sorted set of string contents.
-     *
-     * @param fileContent - filename to read from. Must be a UTF-8 coded text file.
-     * @return - entire text file as sorted set of string
-     * @throws - IOException
-     */
-    /*public static SortedSet<String> getTextFromFile(List<String> fileContent) {
-        SortedSet<String> result = new TreeSet<>();
-
-        //Эта строка нихера не робит!!!
-        //fileContent.stream().map(line -> result.addAll(asList(splitString(line, " "))));
-
-        Object[] objArr = fileContent.stream().map(line -> line.split(" ")).toArray();
-        String[][] stringArray = Arrays.copyOf(objArr, objArr.length, String[][].class);
-        for (String[] item : stringArray) {
-            result.addAll(asList(item));
-        }
-        return result;
-    }*/
+    private static List<String> readFileLines(String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        return Files.readAllLines(path);
+    }
 
     /**
      * Reads string from console
@@ -85,22 +58,4 @@ public class Main {
         inString = scanner.next();
         return inString;
     }
-
-    /**
-     * Method leaves a word with the key phrase
-     */
-    /*public static SortedSet<String> getContains(SortedSet<String> sortedSet, String strContain) {
-        Supplier<SortedSet<String>> supplier = TreeSet::new;
-        String strContainLowerCase = strContain.toLowerCase();
-        return sortedSet.stream()
-                .filter(item -> item.toLowerCase().contains(strContainLowerCase))
-                .collect(Collectors.toCollection(supplier));
-    }*/
-
-    /**
-     * Method displays the elements of the console
-     */
-    /*private static void setOutput(SortedSet<String> sortedSet) {
-        sortedSet.forEach(System.out::println);
-    }*/
 }
